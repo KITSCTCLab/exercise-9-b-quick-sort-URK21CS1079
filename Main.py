edfab5a015f0b159f7d1a176b46db69be4cdd1f9
URK21CS1079 <110010648+URK21CS1079@users.noreply.github.com>
2022-11-06 20:29:12 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,25 +1,30 @@
 from typing import List
 
-def partition(array, low, high):
-    pivot = array[high]
-    i = low - 1
-    for j in range(low, high):
-        if array[j] <= pivot:
-            i = i + 1
-            (array[i], array[j]) = (array[j], array[i])
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
-    return i + 1
+def partition(arr,low,high):
+   i = ( low-1 )
+   pivot = arr[high] # pivot element
+   for j in range(low , high):
+      # If current element is smaller
+      if arr[j] <= pivot:
+         # increment
+         i = i+1
+         arr[i],arr[j] = arr[j],arr[i]
+   arr[i+1],arr[high] = arr[high],arr[i+1]
+   return ( i+1 )
+# sort
+def quick_sort(arr,low,high):
+   if low < high:
+      # index
+      pi = partition(arr,low,high)
+      # sort the partitions
+      quick_sort(arr, low, pi-1)
+      quick_sort(arr, pi+1, high)
+   return arr
 
-def quick_sort(array, low, high):
-    if low < high:
-        pi = partition(array, low, high)
-        quick_sort(array, low, pi - 1)
-        quick_sort(array, pi + 1, high)
-    return array
- 
+
 input_data = input()
 data = []
-for item in input_data.split(','):
+for item in input_data.split(', '):
   if item.isnumeric():
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
